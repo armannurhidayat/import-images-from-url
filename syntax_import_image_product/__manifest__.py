3e59e03823b4ed5bdb7d10fdbc759c2f3085b611
Arman Nur Hidayat <armannurhidayat7@gmail.com>
2020-09-20 14:04:00 +0700

    update manifest

--- syntax_import_image_product/__manifest__.py
+++ syntax_import_image_product/__manifest__.py
@@ -1,30 +1,17 @@
 # -*- coding: utf-8 -*-
+# ©  2020 Syntaxdev
 {
     'name': "Import Product From URL",
-
-    'summary': """
-        Import Images Product From URL
-    """,
+    'summary': "Import Images Product From URL",
     "license": "LGPL-3",
-    'description': """
-        Import Images Product From URL
-    """,
-
+    'description': "Import Images Product From URL",
     'author': "Arman Nur Hidayat",
     'website': "http://www.syntaxdev.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
+    "images": ["static/logo/logo.png"],
     'category': 'Stock',
     'version': '12.0.1',
-
-    # any module necessary for this one to work correctly
     'depends': ['base','stock'],
-
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
         'wizard/import_menu.xml',
     ],
 }
