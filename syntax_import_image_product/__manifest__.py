9ce155e5bcb2d6495a39ea8460cd5eb4c0d662a5
Arman Nur Hidayat <armannurhidayat7@gmail.com>
2020-09-20 13:55:20 +0700

    update manifest

--- syntax_import_image_product/__manifest__.py
+++ syntax_import_image_product/__manifest__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "syntax_import_image_product",
+    'name': "Import Product From URL",
 
     'summary': """
         Import Images Product From URL
     """,
-
+    "license": "LGPL-3",
     'description': """
         Import Images Product From URL
     """,
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Stock',
-    'version': '0.1',
+    'version': '12.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base','stock'],
