a168b195185ff80703c48a5f7407a55c2d57f4bb
Arman Nur Hidayat <armannurhidayat7@gmail.com>
2020-09-20 13:29:07 +0700

    first commit

--- __manifest__.py
+++ __manifest__.py
@@ -3,11 +3,11 @@
     'name': "syntax_import_image_product",
 
     'summary': """
-        Import image product
+        Import Images Product From URL
     """,
 
     'description': """
-        Import image product
+        Import Images Product From URL
     """,
 
     'author': "Arman Nur Hidayat",
@@ -16,7 +16,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Stock',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
